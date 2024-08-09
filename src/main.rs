77a819233f3d24826973e4ec23491e92825fed9f
mnsdojo <manishkumar97430@gmail.com>
2024-08-09 11:37:53 -0400

    added simple runtime with console

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-fn main() {
-    let _runtime = tokio::runtime::Builder::new_current_thread()
+
+
+use std::{env, rc::Rc};
+
+use deno_core::error::AnyError;
+
+async fn run_js(file_path: &str) -> Result<(), AnyError> {
+    let main_module = deno_core::resolve_path(file_path, env::current_dir()?.as_path())?;
+    let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+        module_loader:Some(Rc::new(deno_core::FsModuleLoader)),
+        ..Default::default()
+    });
+    
+
+    let mod_id = js_runtime.load_main_es_module(&main_module).await?;
+    let result = js_runtime.mod_evaluate(mod_id);
+    js_runtime.run_event_loop(Default::default()).await?;
+    result.await
+}
+
+
+ fn main() {
+    let runtime = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .unwrap();
-    
+    if let Err(error) = runtime.block_on(run_js("./example.ts")) {
+        eprintln!("error: {}", error);
+    }
 }
